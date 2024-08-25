24b2e7ce78bc248f4be1db39b1fbb661c0934d02
meriuts <nameriuts@gmail.com>
2024-08-25 12:04:10 +0300

    init

---
+++ src/main/java/ru/skillbox/SocialNetworkGatewayApplication.java
@@ -0,0 +1,13 @@
+package ru.skillbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetworkGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialNetworkGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/skillbox/SocialNetworkGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skillbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
