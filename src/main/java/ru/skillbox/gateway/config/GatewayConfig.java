b5e3d4a61c1344818d1549d07080012d2152888c
nesquik-m <nesti_lash@mail.ru>
2024-09-10 19:39:59 +0300

    changes

--- src/main/java/ru/skillbox/gateway/config/GatewayConfig.java
+++ src/main/java/ru/skillbox/gateway/config/GatewayConfig.java
@@ -1,11 +1,16 @@
 package ru.skillbox.gateway.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import reactor.core.publisher.Mono;
 
 @Configuration
+@Slf4j
 public class GatewayConfig {
 
     @Bean
@@ -25,5 +30,27 @@ public class GatewayConfig {
                                 .uri("lb://SOCIAL-NETWORK-POST"))
                 .build();
     }
+
+    @Bean
+    public GlobalFilter loggingFilter() {
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            log.info("Request: {} {} {}", request.getMethod(), request.getPath(), request.getURI());
+            return chain.filter(exchange).then(Mono.fromRunnable(() ->
+                    log.info("Response: {} {} {}", exchange.getResponse().getStatusCode(), request.getPath(), request.getURI())));
+        };
+    }
+    // INFO ru.skillbox.gateway.config.GatewayConfig - Request: GET /api/v1/account/users http://localhost:8080/api/v1/account/users
+    // INFO ru.skillbox.gateway.config.GatewayConfig - Response: 200 OK /api/v1/account/users http://localhost:8080/api/v1/account/users
+
+//    @Bean
+//    public GlobalFilter loggingFilter() {
+//        return (exchange, chain) -> {
+//            ServerHttpRequest request = exchange.getRequest();
+//            log.info("Request: {} {} {}", request.getMethod(), request.getPath(), request.getURI());
+//            return chain.filter(exchange);
+//        };
+//    }
+//    // INFO  ru.skillbox.gateway.config.GatewayConfig  - Request: GET /api/v1/account/users  http://localhost:8080/api/v1/account/users
 
 }
