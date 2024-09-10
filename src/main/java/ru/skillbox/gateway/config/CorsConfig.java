79566517252887f424620ec39374e7e7bb33cd35
Aleksey Chernov <alexidal@mail.ru>
2024-09-10 21:12:30 +0300

    add cors

---
+++ src/main/java/ru/skillbox/gateway/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package ru.skillbox.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class CorsConfig implements WebFluxConfigurer {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://89.111.174.153:8080"); // Указываем разрешенный источник
+        config.addAllowedMethod("*"); // Разрешаем все методы
+        config.addAllowedHeader("*"); // Разрешаем все заголовки
+        config.setAllowCredentials(true); // Разрешаем отправку учетных данных
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config); // Применяем ко всем маршрутам
+
+        return source;
+    }
+}
+
--- src/main/java/ru/skillbox/gateway/config/WebConfig.java
+++ src/main/java/ru/skillbox/gateway/config/WebConfig.java
@@ -1,18 +1,18 @@
-package ru.skillbox.gateway.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.config.CorsRegistry;
-import org.springframework.web.reactive.config.WebFluxConfigurer;
-
-@Configuration
-public class WebConfig implements WebFluxConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://89.111.174.153:8080")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
-}
+//package ru.skillbox.gateway.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.reactive.config.CorsRegistry;
+//import org.springframework.web.reactive.config.WebFluxConfigurer;
+//
+//@Configuration
+//public class WebConfig implements WebFluxConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://89.111.174.153:8080")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+//                .allowedHeaders("*")
+//                .allowCredentials(true);
+//    }
+//}
